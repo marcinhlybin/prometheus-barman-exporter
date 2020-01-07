18266e4ad6b64c05d2e5720253fb4a9a41b5b7ec
Marcin Hlybin <marcin.hlybin@gmail.com>
2020-01-07 11:39:37 +0100

    CLI version using JSON is the only version now

--- barman_exporter.py
+++ barman_exporter.py
@@ -1,101 +1,55 @@
 #!/usr/bin/env python3
 import sys
-import time
-import logging
 import argparse
+import time
+import json
+from sh import barman as barman_cli
+from datetime import datetime
 import prometheus_client
 from prometheus_client import core
-from barman import cli
-from barman import output
-from barman.infofile import BackupInfo
-from barman.server import CheckOutputStrategy
 
 
-class PythonOutputWriter(output.ConsoleOutputWriter):
+class Barman:
 
-    def close(self):
-        if hasattr(self, 'results'):
-            return self.results
+    @staticmethod
+    def cli(*args, **kwargs):
+        output = barman_cli('-f', 'json', *args, **kwargs)
+        output = json.loads(str(output))
+        return output
 
-    def init_list_backup(self, server_name, minimal=False):
-        self.results = dict(
-            status_done=[],
-            status_failed=[]
-        )
+    def servers(self):
+        servers = self.cli('list-server')
+        return list(servers.keys())
 
-    def result_list_backup(self, backup_info, backup_size, wal_size,
-                           retention_status):
-        if backup_info.status in BackupInfo.STATUS_COPY_DONE:
-            self.results['status_done'].append(dict(
-                server_name=backup_info.server_name,
-                backup_id=backup_info.backup_id,
-                end_time=backup_info.end_time.timestamp(),
-                size=backup_size,
-                wal_size=wal_size))
-        else:
-            self.results['status_failed'].append(dict(
-                server_name=backup_info.server_name,
-                backup_id=backup_info.backup_id))
+    def server_status(self, server_name):
+        status = self.cli('status', server_name)
+        status = { k:v['message'] for k, v in status[server_name].items() }
+        return status
 
-    def init_check(self, server_name, active=True):
-        self.results = {}
-
-    def result_check(self, server_name, check, status, hint=None):
-        check = check.lower() \
-            .replace(' ', '_') \
-            .replace('-', '_') \
-            .replace('.', '')
-
-        self.results[check] = 1 if status else 0
-
-
-class BarmanCollector(object):
-
-    def __init__(self, args):
-        cli.global_config(args)
-
-        # Remove logging handlers to avoid opening
-        # barman.log over and over again
-        logging.disable(logging.CRITICAL)
-        for handler in logging.root.handlers[:]:
-            logging.root.removeHandler(handler)
-
-        self.servers = cli.get_server_list(args)
-        self.server_names = self.servers.keys()
+    def server_check(self, server_name):
+        check = self.cli('check', server_name, _ok_code=[0, 1])
+        check = { k:1 if v['status'] == "OK" else 0 for k, v in check[server_name].items() } 
+        return check
 
     def list_backup(self, server_name):
-        output.init('list_backup', server_name)
+        backups = self.cli('list-backup', server_name)
+        backups_done = [ backup for backup in backups[server_name] if backup['status'] == 'DONE']
+        backups_failed = [ backup for backup in backups[server_name] if backup['status'] != 'DONE']
+        return backups_done, backups_failed
 
-        server = self.servers[server_name]
-        server.list_backups()
-        server.close()
 
-        results = output._writer.close()
-        return results
+class BarmanCollector:
 
-    def check(self, server_name, check_strategy=CheckOutputStrategy()):
-        output.init('check', server_name)
+    def __init__(self, servers):
+        self.servers = servers
 
-        server = self.servers[server_name]
-        server.check_archive(check_strategy)
-
-        if not server.passive_node:
-            server.check_postgres(check_strategy)
-
-        server.check_directories(check_strategy)
-        server.check_retention_policy_settings(check_strategy)
-        server.check_backup_validity(check_strategy)
-        server.backup_manager.check(check_strategy)
-        server.check_configuration(check_strategy)
-
-        for archiver in server.archivers:
-            archiver.check(check_strategy)
-
-        server.check_archiver_errors(check_strategy)
-        server.close()
-
-        results = output._writer.close()
-        return results
+    @staticmethod
+    def pretty_size_to_bytes(size, suffixes="KMGTPEZY"):
+        size, suffix = size.split()
+        unit = 1024 if "iB" in suffix else 1000
+        exponent = suffixes.find(suffix[0].upper()) + 1
+        size_bytes = float(size) * (unit ** exponent)
+        return int(size_bytes)
 
     def collect(self):
         collectors = dict(
@@ -122,49 +76,48 @@ class BarmanCollector(object):
                 labels=["server", "check"])
         )
 
-        for server_name in self.server_names:
-            backups = self.list_backup(server_name)
+        barman = Barman()
+
+        if self.servers[0] == "all":
+            self.servers = barman.servers()
+
+        for server_name in self.servers:
+            server_status = barman.server_status(server_name)
+
+            if server_status['first_backup']:
+                first_backup = datetime.strptime(
+                    server_status['first_backup'], "%Y%m%dT%H%M%S")
+                collectors['barman_first_backup'].add_metric(
+                    [server_name], first_backup.strftime("%s"))
+
+            if server_status['last_backup']:
+                last_backup = datetime.strptime(
+                    server_status['last_backup'], "%Y%m%dT%H%M%S")
+                collectors['barman_last_backup'].add_metric(
+                    [server_name], last_backup.strftime("%s"))
+
+            backups_done, backups_failed = barman.list_backup(server_name)
 
             collectors['barman_backups_total'].add_metric(
-                [server_name],
-                len(backups['status_done']) + len(backups['status_failed']))
+                [server_name], len(backups_done) + len(backups_failed))
 
             collectors['barman_backups_failed'].add_metric(
-                [server_name], len(backups['status_failed']))
+                [server_name], len(backups_failed))
 
-            if backups['status_done']:
-                collectors['barman_last_backup'].add_metric(
-                    [server_name], backups['status_done'][0]['end_time'])
-
-                collectors['barman_first_backup'].add_metric(
-                    [server_name], backups['status_done'][-1]['end_time'])
-
-            for number, backup in enumerate(backups['status_done'], 1):
+            for number, backup in enumerate(backups_done, 1):
                 collectors['barman_backups_size'].add_metric(
-                    [server_name, str(number)], backup['size'])
+                    [server_name, str(number)], backup['size_bytes'])
 
                 collectors['barman_backups_wal_size'].add_metric(
-                    [server_name, str(number)], backup['wal_size'])
+                    [server_name, str(number)], backup['wal_size_bytes'])
 
-            check = self.check(server_name)
-            for check_name, check_value in check.items():
+            server_check = barman.server_check(server_name)
+            for check_name, check_value in server_check.items():
                 collectors['barman_up'].add_metric(
                     [server_name, check_name], check_value)
 
         for collector in collectors.values():
             yield collector
-
-
-class BarmanArgs():
-
-    def __init__(self, servers):
-        output.set_output_writer(PythonOutputWriter())
-        self.quiet = output._writer
-        self.debug = output._writer
-        self.format = output._writer
-        self.color = 'auto'
-        self.minimal = False
-        self.server_name = servers
 
 
 if __name__ == "__main__":
@@ -187,11 +140,7 @@ if __name__ == "__main__":
               args.web_listen_address), "Use HOST:PORT.")
         sys.exit(1)
 
-    barman_args = BarmanArgs(args.servers)
-    collector = BarmanCollector(barman_args)
-
-    core.REGISTRY.register(collector)
+    core.REGISTRY.register(BarmanCollector(args.servers))
     prometheus_client.start_http_server(int(port), addr)
-
     while True:
         time.sleep(1)
