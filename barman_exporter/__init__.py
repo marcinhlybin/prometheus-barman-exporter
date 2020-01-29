f9167a690072568c10ed7c40a31b7e92191b79c2
Marcin Hlybin <marcin.hlybin@gmail.com>
2020-01-29 15:31:55 +0100

    Fix pip package

--- barman_exporter/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import barman_exporter
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='barman_exporter',
-      version='1.0.3',
+      version='1.0.4',
       description='Barman exporter for Prometheus',
       long_description='Barman exporter for Prometheus. Full description at https://github.com/ahes/prometheus-barman-exporter',
       url='https://github.com/ahes/prometheus-barman-exporter',
@@ -11,7 +11,7 @@ setup(name='barman_exporter',
       packages=['barman_exporter'],
       keywords='prometheus barman exporter barman-exporter',
       entry_points = {
-        'console_scripts': ['barman-exporter=barman_exporter:main'],
+        'console_scripts': ['barman-exporter=barman_exporter.barman_exporter:main'],
       },
       install_requires=[
         'prometheus-client',
